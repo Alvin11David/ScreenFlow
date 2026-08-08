d32a6fd240b8eff349cc625ecb321b3c1c3d9abf
Alvin11David <alvin69david@gmail.com>
2026-08-08 09:17:02 +0300

    auto: update 2026-08-08 08:23:15

--- lib/db/dist_bak2/schema/index.d.ts
+++
@@ -1,9 +0,0 @@
-export * from "./users";
-export * from "./sessions";
-export * from "./videos";
-export * from "./video-shares";
-export * from "./video-analytics";
-export * from "./teams";
-export * from "./team-members";
-export * from "./subscriptions";
-//# sourceMappingURL=index.d.ts.map
